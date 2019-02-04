143f619bc9648ce2dabe925d009276c35e3173d3
Thomas Lin Pedersen <thomasp85@gmail.com>
2019-02-04 11:52:11 +0100

    fix indentation

--- src/grid.c
+++ src/grid.c
@@ -1136,7 +1136,7 @@ SEXP L_convert(SEXP x, SEXP whatfrom,
          * (to avoid divide-by-zero, but still do something useful)
          */
         relConvert = (!isUnitArithmetic(x) && !isUnitList(x) &&
-        	(unitUnit(x, i) == L_NATIVE || unitUnit(x, i) == L_NPC) &&
+                      (unitUnit(x, i) == L_NATIVE || unitUnit(x, i) == L_NPC) &&
                       (TOunit == L_NATIVE || TOunit == L_NPC) &&
                       ((FROMaxis == TOaxis) ||
                        (FROMaxis == 0 && TOaxis == 2) ||
